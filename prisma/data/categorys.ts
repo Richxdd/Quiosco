bdf72e6123ae48c324354d743dd67bbe75d6b291
RICARDO SOLIS <ricardosv46@gmail.com>
2022-05-14 02:30:37 -0500

    feat

--- prisma/data/categorys.ts
+++ prisma/data/categorys.ts
@@ -1,10 +1,10 @@
 const categorys = [
-  { icon: 'cafe', name: 'Café' },
-  { icon: 'hamburguesa', name: 'Hamburguesas' },
-  { icon: 'pizza', name: 'Pizzas' },
-  { icon: 'dona', name: 'Donas' },
-  { icon: 'pastel', name: 'Pasteles' },
-  { icon: 'galletas', name: 'Galletas' }
+  { id: 1, icon: 'cafe', name: 'Café' },
+  { id: 2, icon: 'hamburguesa', name: 'Hamburguesas' },
+  { id: 3, icon: 'pizza', name: 'Pizzas' },
+  { id: 4, icon: 'dona', name: 'Donas' },
+  { id: 5, icon: 'pastel', name: 'Pasteles' },
+  { id: 6, icon: 'galletas', name: 'Galletas' }
 ]
 
 export { categorys }
